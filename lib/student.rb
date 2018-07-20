1c6a0840dd64cf8e500b5893a34ef95183da80ae
Tao <werewaswas@live.com>
2018-07-20 07:08:30 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,10 +1,10 @@
 class Student
   def hello
-    p "Hey there! I'm so excited to learn stuff.\n"
+    p "Hey there! I'm so excited to learn stuff."
   end
   
   def raise_hand
-    p "Pick me!\n"
+    p "Pick me!"
   end
   
 end
